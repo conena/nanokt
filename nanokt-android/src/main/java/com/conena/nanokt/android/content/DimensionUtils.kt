1a0e7eccbd3ae8793e516ed1710f6576e428b14f
Fabian Andera <fabian@conena.com>
2022-09-30 01:19:37 +0200

    Added dimension properties and methods for float type

--- nanokt-android/src/main/java/com/conena/nanokt/android/content/DimensionUtils.kt
+++ nanokt-android/src/main/java/com/conena/nanokt/android/content/DimensionUtils.kt
@@ -70,3 +70,65 @@ inline fun Int.pxToSp() = this.div(Resources.getSystem().displayMetrics.scaledDe
 inline fun Int.toPx(unit: Int): Int {
     return TypedValue.applyDimension(unit, toFloat(), Resources.getSystem().displayMetrics).roundToInt()
 }
+
+/**
+ * The current value interpreted as density-independent pixels and converted to pixels.
+ */
+inline val Float.dpInPx get() = this.times(Resources.getSystem().displayMetrics.density).roundToInt()
+
+/**
+ * The current value interpreted as pixels and converted to density-independent pixels.
+ */
+inline val Float.pxInDp get() = this.div(Resources.getSystem().displayMetrics.density).roundToInt()
+
+/**
+ * The current value interpreted as scale-independent pixels and converted to pixels.
+ */
+inline val Float.spInPx get() = this.times(Resources.getSystem().displayMetrics.scaledDensity).roundToInt()
+
+/**
+ * The current value interpreted as pixels and converted to scale-independent pixels.
+ */
+inline val Float.pxInSp get() = this.div(Resources.getSystem().displayMetrics.scaledDensity).roundToInt()
+
+/**
+ * @return Convert the current value from density-independent pixels to pixels.
+ */
+@CheckResult
+@Px
+inline fun Float.dpToPx() = this.times(Resources.getSystem().displayMetrics.density).roundToInt()
+
+/**
+ * @return Convert the current value from pixels to density-independent pixels.
+ */
+@CheckResult
+inline fun Float.pxToDp() = this.div(Resources.getSystem().displayMetrics.density).roundToInt()
+
+/**
+ * @return Convert the current value from scale-independent pixels to pixels.
+ */
+@CheckResult
+@Px
+inline fun Float.spToPx() = this.times(Resources.getSystem().displayMetrics.scaledDensity).roundToInt()
+
+/**
+ * @return Convert the current value from pixels to scale-independent pixels.
+ */
+@CheckResult
+inline fun Float.pxToSp() = this.div(Resources.getSystem().displayMetrics.scaledDensity).roundToInt()
+
+/**
+ * @param unit The unit to convert the value from. The possible values are listed below.
+ * @return The current value treated as [unit] converted to pixels.
+ * @see TypedValue.COMPLEX_UNIT_PX
+ * @see TypedValue.COMPLEX_UNIT_DIP
+ * @see TypedValue.COMPLEX_UNIT_SP
+ * @see TypedValue.COMPLEX_UNIT_PT
+ * @see TypedValue.COMPLEX_UNIT_IN
+ * @see TypedValue.COMPLEX_UNIT_MM
+ */
+@CheckResult
+@Px
+inline fun Float.toPx(unit: Int): Int {
+    return TypedValue.applyDimension(unit, this, Resources.getSystem().displayMetrics).roundToInt()
+}
