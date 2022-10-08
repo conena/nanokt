8d5cd4403dcbe49f9165aa2cffe0d6054c9ab840
Fabian Andera <fabian@conena.com>
2022-10-08 16:36:11 +0200

    Improved getSuitableDateTimeFormat

--- nanokt-android/src/main/java/com/conena/nanokt/android/text/FormatUtils.kt
+++ nanokt-android/src/main/java/com/conena/nanokt/android/text/FormatUtils.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.text.format.DateFormat
 import androidx.annotation.CheckResult
 import androidx.annotation.RequiresApi
+import com.conena.nanokt.android.ExperimentalNanoKtAndroidApi
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -16,7 +17,9 @@ import java.util.*
  * @param locale The locale into which the skeleton should be localized.
  * @return The best possible localized [SimpleDateFormat] of the given skeleton for the given locale.
  * @see DateFormat.getBestDateTimePattern
+ * @see Locale.getSuitableDateTimeFormat
  */
+@ExperimentalNanoKtAndroidApi
 @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 @CheckResult
 inline fun getSuitableDateTimeFormat(
@@ -24,4 +27,15 @@ inline fun getSuitableDateTimeFormat(
     locale: Locale = Locale.getDefault()
 ): SimpleDateFormat {
     return SimpleDateFormat(DateFormat.getBestDateTimePattern(locale, skeleton), locale)
+}
+
+/**
+ * @param skeleton A skeleton similar to, and uses the same format characters as, a Unicode
+ * [UTS#35 Pattern](http://www.unicode.org/reports/tr35/#Date_Format_Patterns).
+ * @return The best possible localized [SimpleDateFormat] of the given skeleton for the receiver.
+ */
+@RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+@CheckResult
+inline fun Locale.getSuitableDateTimeFormat(skeleton: String): SimpleDateFormat {
+    return SimpleDateFormat(DateFormat.getBestDateTimePattern(this, skeleton), this)
 }
