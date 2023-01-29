23a347adfe4f99bbb6c9c5e7c241a9b14e9cea89
Fabian Andera <fabian@conena.com>
2023-01-29 13:59:52 +0100

    Move ThreadUtils to separate module

--- nanokt-android-threads/src/main/java/com/conena/nanokt/threads/android/os/ThreadUtils.kt
+++ nanokt-android-threads/src/main/java/com/conena/nanokt/threads/android/os/ThreadUtils.kt
@@ -18,20 +18,18 @@
 @file:JvmName(name = "ThreadUtils")
 @file:Suppress("NOTHING_TO_INLINE", "unused")
 
-package com.conena.nanokt.android.os
+package com.conena.nanokt.threads.android.os
 
 
 import android.os.Handler
 import android.os.Looper
 import androidx.annotation.MainThread
 import androidx.annotation.RestrictTo
-import com.conena.nanokt.android.ExperimentalNanoKtAndroidApi
 import java.util.concurrent.TimeUnit
 
 /**
  * ThreadUtils companion object to avoid creating new [Handler] instances again and again.
  */
-@ExperimentalNanoKtAndroidApi
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 object NanoKtThreadUtilsCompanion {
     val mainHandler: Handler by lazy { Handler(Looper.getMainLooper()) }
@@ -40,7 +38,6 @@ object NanoKtThreadUtilsCompanion {
 /**
  * Invokes [block] on the main thread.
  */
-@ExperimentalNanoKtAndroidApi
 inline fun runOnMainThread(@MainThread block: Runnable) {
     if (Looper.myLooper() == Looper.getMainLooper()) {
         block.run()
@@ -52,7 +49,6 @@ inline fun runOnMainThread(@MainThread block: Runnable) {
 /**
  * Invokes [block] on the main thread after the specified [delay] in the specified [unit].
  */
-@ExperimentalNanoKtAndroidApi
 inline fun runOnMainThread(
     delay: Long,
     unit: TimeUnit = TimeUnit.MILLISECONDS,
@@ -64,7 +60,6 @@ inline fun runOnMainThread(
 /**
  * Invokes the receiver on the main thread.
  */
-@ExperimentalNanoKtAndroidApi
 inline fun Function0<Unit>.invokeOnMainThread() {
     if (Looper.myLooper() == Looper.getMainLooper()) {
         invoke()
@@ -76,7 +71,6 @@ inline fun Function0<Unit>.invokeOnMainThread() {
 /**
  * Invokes the receiver on the main thread.
  */
-@ExperimentalNanoKtAndroidApi
 inline fun <P1> Function1<P1, Unit>.invokeOnMainThread(p1: P1) {
     if (Looper.myLooper() == Looper.getMainLooper()) {
         invoke(p1)
@@ -90,7 +84,6 @@ inline fun <P1> Function1<P1, Unit>.invokeOnMainThread(p1: P1) {
 /**
  * Invokes the receiver on the main thread.
  */
-@ExperimentalNanoKtAndroidApi
 inline fun <P1, P2> Function2<P1, P2, Unit>.invokeOnMainThread(p1: P1, p2: P2) {
     if (Looper.myLooper() == Looper.getMainLooper()) {
         invoke(p1, p2)
@@ -104,7 +97,6 @@ inline fun <P1, P2> Function2<P1, P2, Unit>.invokeOnMainThread(p1: P1, p2: P2) {
 /**
  * Invokes the receiver on the main thread.
  */
-@ExperimentalNanoKtAndroidApi
 inline fun <P1, P2, P3> Function3<P1, P2, P3, Unit>.invokeOnMainThread(p1: P1, p2: P2, p3: P3) {
     if (Looper.myLooper() == Looper.getMainLooper()) {
         invoke(p1, p2, p3)
@@ -118,7 +110,6 @@ inline fun <P1, P2, P3> Function3<P1, P2, P3, Unit>.invokeOnMainThread(p1: P1, p
 /**
  * Invokes the receiver on the main thread.
  */
-@ExperimentalNanoKtAndroidApi
 inline fun <P1, P2, P3, P4> Function4<P1, P2, P3, P4, Unit>.invokeOnMainThread(p1: P1, p2: P2, p3: P3, p4: P4) {
     if (Looper.myLooper() == Looper.getMainLooper()) {
         invoke(p1, p2, p3, p4)
@@ -132,7 +123,6 @@ inline fun <P1, P2, P3, P4> Function4<P1, P2, P3, P4, Unit>.invokeOnMainThread(p
 /**
  * Invokes the receiver on the main thread.
  */
-@ExperimentalNanoKtAndroidApi
 inline fun <P1, P2, P3, P4, P5> Function5<P1, P2, P3, P4, P5, Unit>.invokeOnMainThread(p1: P1, p2: P2, p3: P3, p4: P4, p5: P5) {
     if (Looper.myLooper() == Looper.getMainLooper()) {
         invoke(p1, p2, p3, p4, p5)
