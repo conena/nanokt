7151004170fb0fdc4f5989c7cec6c9d58662f75a
Fabian Andera <fabian@conena.com>
2023-08-14 11:41:47 +0200

    Add float extensions

---
+++ nanokt/src/main/java/com/conena/nanokt/numbers/FloatExtensions.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2023 Fabian Andera
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+@file:Suppress("NOTHING_TO_INLINE", "unused")
+
+package com.conena.nanokt.numbers
+
+/**
+ * @return `null` if the receiver is zero. Otherwise the receiver is returned.
+ */
+inline fun Float.zeroAsNull() = if (this == 0f) null else this
+
+/**
+ * @return 0 if the receiver is `null`. Otherwise the receiver is returned.
+ */
+inline fun Float?.nullAsZero() = this ?: 0f
+
+/**
+ * @return `null` if the receiver is negative. Otherwise the receiver is returned.
+ */
+inline fun Float.negativeAsNull() = if (this < 0f) null else this
+
+/**
+ * @return 0 if the receiver is negative. Otherwise the receiver is returned.
+ */
+inline fun Float.negativeAsZero() = if (this < 0f) 0f else this
+
+/**
+ * @return `null` if the receiver is positive. Otherwise the receiver is returned.
+ */
+inline fun Float.positiveAsNull() = if (this > 0f) null else this
+
+/**
+ * @return 0 if the receiver is positive. Otherwise the receiver is returned.
+ */
+inline fun Float.positiveAsZero() = if (this > 0f) 0f else this
+
+/**
+ * @return `true` if the receiver is not `null` and negative.
+ */
+inline fun Float?.isNegative() = this != null && this < 0f
+
+/**
+ * @return `true` if the receiver is not `null` and positive.
+ */
+inline fun Float?.isPositive() = this != null && this > 0f
+
+/**
+ * @return `true` it the receiver is zero.
+ */
+inline fun Float?.isZero() = this == 0f
+
+/**
+ * @return `true` if the receiver is not negative.
+ */
+inline fun Float?.isNotNegative() = this == null || this >= 0f
+
+/**
+ * @return `true` if the receiver is not positive.
+ */
+inline fun Float?.isNotPositive() = this == null || this <= 0f
+
+/**
+ * @return `true` it the receiver is not zero.
+ */
+inline fun Float?.isNotZero() = this != 0f
+
+/**
+ * @return `true` if the receiver is `null` or negative.
+ */
+inline fun Float?.isNullOrNegative() = this == null || this < 0f
+
+/**
+ * @return `true` if the receiver is `null` or positive.
+ */
+inline fun Float?.isNullOrPositive() = this == null || this > 0f
+
+/**
+ * @return `true` it the receiver is `null` or zero.
+ */
+inline fun Float?.isNullOrZero() = this == null || this == 0f
---
+++ nanokt/src/test/java/com/conena/nanokt/numbers/FloatExtensionsKtTest.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2023 Fabian Andera
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.conena.nanokt.numbers
+
+import org.junit.Assert
+import org.junit.Test
+
+class FloatExtensionsKtTest {
+
+    @Test
+    fun zeroAsNull() {
+        Assert.assertNull(0.0f.zeroAsNull())
+        Assert.assertEquals(0.1f, 0.1f.zeroAsNull())
+        Assert.assertEquals(-0.1f, (-0.1f).zeroAsNull())
+    }
+
+    @Test
+    fun nullAsZero() {
+        val nullValue: Float? = null
+        Assert.assertEquals(0.0f, nullValue.nullAsZero())
+        Assert.assertEquals(0.0f, 0.0f.nullAsZero())
+        Assert.assertEquals(0.1f, 0.1f.nullAsZero())
+        Assert.assertEquals(-0.1f, (-0.1f).nullAsZero())
+    }
+
+    @Test
+    fun negativeAsNull() {
+        Assert.assertEquals(0.0f, 0.0f.negativeAsNull())
+        Assert.assertEquals(0.1f, 0.1f.negativeAsNull())
+        Assert.assertNull((-0.1f).negativeAsNull())
+    }
+
+    @Test
+    fun negativeAsZero() {
+        Assert.assertEquals(0.0f, 0.0f.negativeAsZero())
+        Assert.assertEquals(0.1f, 0.1f.negativeAsZero())
+        Assert.assertEquals(0.0f, (-0.1f).negativeAsZero())
+    }
+
+    @Test
+    fun positiveAsNull() {
+        Assert.assertEquals(0.0f, 0.0f.positiveAsNull())
+        Assert.assertNull(0.1f.positiveAsNull())
+        Assert.assertEquals(-0.1f, (-0.1f).positiveAsNull())
+    }
+
+    @Test
+    fun positiveAsZero() {
+        Assert.assertEquals(0.0f, 0.0f.positiveAsZero())
+        Assert.assertEquals(0.0f, 0.1f.positiveAsZero())
+        Assert.assertEquals(-0.1f, (-0.1f).positiveAsZero())
+    }
+
+    @Test
+    fun isNegative() {
+        val nullValue: Float? = null
+        Assert.assertFalse(nullValue.isNegative())
+        Assert.assertFalse(0.0f.isNegative())
+        Assert.assertFalse(0.1f.isNegative())
+        Assert.assertTrue((-0.1f).isNegative())
+    }
+
+    @Test
+    fun isPositive() {
+        val nullValue: Float? = null
+        Assert.assertFalse(nullValue.isPositive())
+        Assert.assertFalse(0.0f.isPositive())
+        Assert.assertTrue(0.1f.isPositive())
+        Assert.assertFalse((-0.1f).isPositive())
+    }
+
+    @Test
+    fun isZero() {
+        val nullValue: Float? = null
+        Assert.assertFalse(nullValue.isZero())
+        Assert.assertTrue(0.0f.isZero())
+        Assert.assertFalse(0.1f.isZero())
+        Assert.assertFalse((-0.1f).isZero())
+    }
+
+    @Test
+    fun isNotNegative() {
+        val nullValue: Float? = null
+        Assert.assertTrue(nullValue.isNotNegative())
+        Assert.assertTrue(0.0f.isNotNegative())
+        Assert.assertTrue(0.1f.isNotNegative())
+        Assert.assertFalse((-0.1f).isNotNegative())
+    }
+
+    @Test
+    fun isNotPositive() {
+        val nullValue: Float? = null
+        Assert.assertTrue(nullValue.isNotPositive())
+        Assert.assertTrue(0.0f.isNotPositive())
+        Assert.assertFalse(0.1f.isNotPositive())
+        Assert.assertTrue((-0.1f).isNotPositive())
+    }
+
+    @Test
+    fun isNotZero() {
+        val nullValue: Float? = null
+        Assert.assertTrue(nullValue.isNotZero())
+        Assert.assertFalse(0.0f.isNotZero())
+        Assert.assertTrue(0.1f.isNotZero())
+        Assert.assertTrue((-0.1f).isNotZero())
+    }
+
+    @Test
+    fun isNullOrNegative() {
+        val nullValue: Float? = null
+        Assert.assertTrue(nullValue.isNullOrNegative())
+        Assert.assertFalse(0.0f.isNullOrNegative())
+        Assert.assertFalse(0.1f.isNullOrNegative())
+        Assert.assertTrue((-0.1f).isNullOrNegative())
+    }
+
+    @Test
+    fun isNullOrPositive() {
+        val nullValue: Float? = null
+        Assert.assertTrue(nullValue.isNullOrPositive())
+        Assert.assertFalse(0.0f.isNullOrPositive())
+        Assert.assertTrue(0.1f.isNullOrPositive())
+        Assert.assertFalse((-0.1f).isNullOrPositive())
+    }
+
+    @Test
+    fun isNullOrZero() {
+        val nullValue: Float? = null
+        Assert.assertTrue(nullValue.isNullOrZero())
+        Assert.assertTrue(0.0f.isNullOrZero())
+        Assert.assertFalse(0.1f.isNullOrZero())
+        Assert.assertFalse((-0.1f).isNullOrZero())
+    }
+
+}
